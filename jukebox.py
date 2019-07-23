11e67efdc89eadb4006d4e8437fe2ea665a4e1e2
Comten <nicolas.comte@inria.fr>
2019-07-24 00:03:29 +0200

    Initial commit

---
+++ jukebox.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 23 22:33:10 2019
+
+@author: comten
+"""
+
+import sys
+
+def main(argv):
+    print("Hello world")
+
+
+if __name__ == '__main__':
+    main(sys.argv)
