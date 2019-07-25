c61208701459d69062f6548f7da950c41a47f9f3
Comten <nicolas.comte@inria.fr>
2019-07-25 18:07:28 +0200

    Update main.py to get

--- main.py
+++ main.py
@@ -3,14 +3,27 @@
 """
 Created on Tue Jul 23 22:33:10 2019
 
-@author: comten
+@author: Nicolas Comte
 """
 
 import sys
+from lib.jukebox import *
+
+queen = [
+            Track('The Prophet\'s Song', 'Brian May', 3),
+            Track('Love of My Life', 'Freddie Mercury', 1),
+            Track('Good Company', 'Brian May', 1),
+            Track('Bohemian Rhapsody', 'Freddie Mercury', 2),
+            Track('God Save the Queen', '-', 1)
+        ]
 
 def main(argv):
-    print("Hello world")
-
+    print('Hello world')
+    my_jukebox = Jukebox(queen) # our jukebox has the album "Queen"
+    my_jukebox.select(queen[1]) # We select the tracks 1, 3 and 4
+    my_jukebox.select(queen[3])
+    my_jukebox.select(queen[4])
+    my_jukebox.play()
 
 if __name__ == '__main__':
     main(sys.argv)
